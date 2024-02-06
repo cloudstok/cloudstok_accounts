0bced86c187d4a17d71e4ba783aca584103dc496
deepakChauhanCloud <deepak.chauhan@cloudstok.com>
2024-02-06 12:04:48 +0530

    chnages in billing

--- controllers/billing.js
+++ controllers/billing.js
@@ -1,7 +1,7 @@
 const { read, write } = require('../db/db-config')
 
 const SQL_GET_BILLING_DATA =  `select * from billing where is_deleted = 0`
-const SQL_INSERT_BILLING_DATA = `insert into billing(billing_invoice-no, billing_date, billing_delivery_note, billing_terms_of_payment, billing_other_reference, billing_buyer_order_number, billing_dispatch_doc_no, billing_delivery_note_date, billing_dispatched_throught, billing_destination, billing_terms_of_delivery, order ) values (?,?,?,?,?,?,?,?,?,?,?,?,?) `
+const SQL_INSERT_BILLING_DATA = `insert into billing(billing_invoice-no, billing_date, billing_delivery_note, billing_terms_of_payment, billing_other_reference, billing_buyer_order_number, billing_dispatch_doc_no, billing_delivery_note_date, billing_dispatched_throught, billing_destination, billing_terms_of_delivery, order, total_amount, amount_in_words ) values (?,?,?,?,?,?,?,?,?,?,?,?,?) `
 const SQL_UPDATE_BILLING_DATA = `update billing set `
 const SQL_DELETE_BILLING_DATA = `delete billing set is_deleted = 1`
 
@@ -35,8 +35,8 @@ const addBilling = async (req, res) => {
     try {
         const {user_type, user_id  } = res.locals.auth.user;
        if(user_type === "admin"){
-        const { invoice_no, invoice_date, delivery_note, mode_payment, other_reference, dis_throught, buyer_order_no, dispatched_no, dated,  delivery_note_date, designation, terms_of_del, productDetails, total_amount} = req.body;
-        await write.query(SQL_INSERT_BILLING_DATA, [billing_invoice-no, billing_date, billing_delivery_note, billing_terms_of_payment, billing_other_reference, billing_dispatched_throught, billing_buyer_order_number, billing_dispatch_doc_no, billing_date, billing_delivery_note, billing_destination, billing_terms_of_delivery, JSON.stringify(order),  ]);
+        const { invoice_no, invoice_date, delivery_note, mode_payment, other_reference, dis_throught, buyer_order_no, dispatched_no, dated,  delivery_note_date, designation, terms_of_del, total_amount, amount_in_words} = req.body;
+        await write.query(SQL_INSERT_BILLING_DATA, [billing_invoice-no, billing_date, billing_delivery_note, billing_terms_of_payment, billing_other_reference, billing_dispatched_throught, billing_buyer_order_number, billing_dispatch_doc_no, billing_date,  billing_delivery_note, billing_destination, billing_terms_of_delivery, JSON.stringify(order), total_amount, amount_in_words  ]);
         return res.status(200).send({ status: "success", msg: `Billing insert successfully`});
        }
         else {
