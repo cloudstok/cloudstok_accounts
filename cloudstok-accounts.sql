111ed51cb0380c0c232dddd0ee19db4d504695e4
deepakChauhanCloud <deepak.chauhan@cloudstok.com>
2024-02-06 11:57:17 +0530

    changes

--- cloudstok-accounts.sql
+++ cloudstok-accounts.sql
@@ -61,14 +61,14 @@ CREATE TABLE if not exists `customer` (
    `billing_destination` varchar(255) default null,
    `billing_terms_of_delivery` varchar(255) default null,
     `order` json, 
-    `total_amount` int default not null,
-    `amount_in_words` varchar(255) defaultnot null,
+    `total_amount` int not null,
+    `amount_in_words` varchar(255) not  null,
    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    `is_deleted` tinyint(1) DEFAULT '0',
    PRIMARY KEY (`billing_id`),
    foreign key(`customer_id`) references customer(`customer_id`),
-   foreign key(`support_id`) references support(`support_id`)   
+   foreign key(`created_by`) references user(`user_id`)   
  );
  
  
