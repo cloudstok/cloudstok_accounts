273719ebbdff756513ca47d3057160b6aabf7605
deepakChauhanCloud <deepak.chauhan@cloudstok.com>
2024-02-08 11:19:36 +0530

    metadata

--- cloudstok-accounts.sql
+++ cloudstok-accounts.sql
@@ -104,13 +104,13 @@ CREATE TABLE if not exists `customer` (
  INSERT IGNORE INTO billing_info(billing_info_meta_data, created_by) values('{
   "amount_key": "Amount Chargeable (in words)",
   "Declaration": "We declare that this invoice shows the actual price of the goods, described and that all particulars are true and,correct.",
-  "company_gst": "GSTIN/UIN: 09AAICC9582C1ZD",
-  "company_emai": "E-Mail : accounts@cloudstok.com",
+  "company_gst": 09AAICC9582C1ZD",
+  "company_email": accounts@cloudstok.com",
   "company_name": "Cloudstok Technologies Private Limited",
   "company_Branch": "Sector 62 Noida",
   "company_address": "101, C-76, SECTOR-63, NOIDA,GAUTAM BUDDHA NAGAR,UTTAR PRADESH",
   "company_IFS_Code": "UTIB0000723",
   "company_State_Name": "Uttar Pradesh, Code : 09",
-  "compnay_bank_details": "Companys Bank Details, Bank Name : Axis Bank",
+  "compnay_bank_details":  "Axis Bank",
   "company_address_A/c No.": 920020046431100
 }', '1');
